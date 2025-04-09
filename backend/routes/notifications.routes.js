208bb5ad6aa8ff69dea9d3aaaa109d72c0fcbfba
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2025-04-09 18:56:33 +0300

    Delete backend directory

--- backend/routes/notifications.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const notificationsController = require("../controllers/notifications.controller");
-const protectRoute = require("../middleware/protectRoute");
-
-route.get("/", protectRoute, notificationsController.getNotifications);
-route.delete("/", protectRoute, notificationsController.deleteNotifications);
-
-module.exports = route;
