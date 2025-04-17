31d04a303a9af34a0e381ff4e726faa758bd26a2
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2025-04-17 19:14:49 +0300

    Update generateTokenAndSetCookie.js

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -1,13 +1,16 @@
 const jwt = require("jsonwebtoken");
+
 const generateTokenAndSetCookie = (userId, res, req) => {
-  console.log(777777)
   const token = jwt.sign({ userId }, process.env.SECRET, {
-     expiresIn: "15d",
+    expiresIn: "15d",
   });
-  res.cookie("jwt", token, { httpOnly: true,
+console.log(777777);
+  res.cookie("jwt", token, {
+    httpOnly: true,
     secure: true,
     sameSite: "None",
-    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 يوم بالميلي ثانية});
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 يوم بالميلي ثانية
+  });
 };
 
 module.exports = generateTokenAndSetCookie;
