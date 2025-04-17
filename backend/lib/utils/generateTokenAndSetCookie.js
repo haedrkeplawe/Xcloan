b67f6d70eea467b609fc5b26bb9bd8d60fc758ba
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2025-04-17 19:04:38 +0300

    Update generateTokenAndSetCookie.js

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const generateTokenAndSetCookie = (userId, res, req) => {
   const token = jwt.sign({ userId }, process.env.SECRET, {
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     sameSite: "None",
     expiresIn: "15d",
